fc7006052c9cd611d3eca651d6a194dda6db9f9c
ChrisWarwick <registrations@nuney.com>
2017-07-18 12:27:04 +0100

    Fixed bug with quote file path

--- Get-Quote.psm1
+++ Get-Quote.psm1
@@ -6,7 +6,7 @@ Chris Warwick, @cjwarwickps, January 2012.  This version, November 2015.
 Read a Quote/Cookie/Fortune/MOTD from a file and display it.
 
 The fortunes file consists of thousands of random quotes.  As a quote can contain
-newline characters, each individual quote is delimited with a 0x00 character; the 
+newline characters, each individual quote is delimited with a 0x00 character; the
 delimiter is also present as the first and last character in the file in order to
 simplify the parsing below.
 
@@ -16,12 +16,12 @@ file. A quotes is then selected randomly and extracted from the resutling data.
 
 Bugs: There's no error checking.  The quote file must be ASCII encoded.
 
-Historical Note: The quote file used here is just over 600kB in size. Back in the 
+Historical Note: The quote file used here is just over 600kB in size. Back in the
 day this could have take some considerable time to read from (slow) disk. Now, of
 course, with tablets having GBs of memory and SSDs, this size is trivial and the
 effort of attempting to read the file efficiently as opposed to simply grabbing the
 whole thing with Get-Content is academic.  However, the techniques may still be of
-value if truly large files are being processed... 
+value if truly large files are being processed...
 
 #>
 
@@ -45,7 +45,7 @@ Function Get-Quote {
 [OutputType([String])]
 [Alias('Cookie')]
 Param (
-    $QuoteFile = '.\Fortunes.dat'
+    $QuoteFile = "$PSScriptRoot\Fortunes.dat"
 )
 
     # Use a filestream to randomly read the quote file
@@ -61,19 +61,19 @@ Param (
     # Read a number of characters that's quick but which will include at least one whole quote.
     # Attempt to read approx 8kB worth of text (but don't try to read more than the size of the file)
 
-    $CharactersToRead = [Math]::Min(8000,$FileStream.Length-1)   
+    $CharactersToRead = [Math]::Min(8000,$FileStream.Length-1)
     $Buffer = New-Object -TypeName Byte[] -ArgumentList $CharactersToRead
 
     # Pick some random place in the quote file, go there and read characters...
     $SeekPosition = Get-Random ($FileStream.Length-$CharactersToRead)
-    [Void]$FileStream.Seek($SeekPosition,[System.IO.SeekOrigin]::Begin) 
+    [Void]$FileStream.Seek($SeekPosition,[System.IO.SeekOrigin]::Begin)
     [Void]$FileStream.Read($Buffer,0,$CharactersToRead)
 
     # Convert the bytes into a string of ASCII characters...
-    $Quotes = [System.Text.Encoding]::ASCII.GetString($Buffer) 
+    $Quotes = [System.Text.Encoding]::ASCII.GetString($Buffer)
 
-    # Quotes are delimited by 0x00 characters; discard any partial quotes on the front and end 
-    # of the string, then split the remainder into individual quotes and pick one at random. 
+    # Quotes are delimited by 0x00 characters; discard any partial quotes on the front and end
+    # of the string, then split the remainder into individual quotes and pick one at random.
     # NB: In the regex, \A matches the beginning of string and \z matches the very end (even after line breaks)
 
     $Quotes -Replace '(?s)\A.*?\x00' -Replace '(?s)\x00[^\x00]*\z' -Split "`0" | Get-Random
